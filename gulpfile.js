c0fd09312d9939d0d96a74dc8a0e8153a1543e06
AlexHotBogomol <Zakablukov777@gmail.com>
2019-12-18 11:55:24 +0200

    add babel"

--- gulpfile.js
+++ gulpfile.js
@@ -4,143 +4,150 @@ const { src, dest } = require("gulp");
 const gulp = require("gulp");
 const autoprefixer = require("gulp-autoprefixer");
 const cssbeautify = require("gulp-cssbeautify");
-const removeComments = require('gulp-strip-css-comments');
+const removeComments = require("gulp-strip-css-comments");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass");
 const cssnano = require("gulp-cssnano");
 const rigger = require("gulp-rigger");
+const babel = require("gulp-babel");
 const uglify = require("gulp-uglify");
 const plumber = require("gulp-plumber");
 const imagemin = require("gulp-imagemin");
 const del = require("del");
 const browsersync = require("browser-sync").create();
 
-
-
 /* Paths to source/build/watch files
 =========================*/
 
 var path = {
-    build: {
-        html: "dist/",
-        js: "dist/assets/js/",
-        css: "dist/assets/css/",
-        images: "dist/assets/i/",
-        fonts: "dist/assets/fonts/",
-        videos: "dist/assets/videos/",
-    },
-    src: {
-        html: "src/*.{htm,html,php}",
-        js: "src/assets/js/*.js",
-        css: "src/assets/sass/style.scss",
-        images: "src/assets/i/**/*.{jpg,png,svg,gif,ico}",
-        fonts: "src/assets/fonts/**/*.{ttf,eot,woff,woff2}",
-        videos: "src/assets/videos/*",
-    },
-    watch: {
-        html: "src/**/*.{htm,html,php}",
-        js: "src/assets/js/**/*.js",
-        css: "src/assets/sass/**/*.scss",
-        images: "src/assets/i/**/*.{jpg,png,svg,gif,ico}",
-        fonts: "src/assets/fonts/**/*.{ttf,eot,woff,woff2}",
-        videos: "src/assets/videos/*",
-    },
-    clean: "./dist"
+  build: {
+    html: "dist/",
+    js: "dist/assets/js/",
+    css: "dist/assets/css/",
+    images: "dist/assets/i/",
+    fonts: "dist/assets/fonts/",
+    videos: "dist/assets/videos/"
+  },
+  src: {
+    html: "src/*.{htm,html,php}",
+    js: "src/assets/js/*.js",
+    css: "src/assets/sass/style.scss",
+    images: "src/assets/i/**/*.{jpg,png,svg,gif,ico}",
+    fonts: "src/assets/fonts/**/*.{ttf,eot,woff,woff2}",
+    videos: "src/assets/videos/*"
+  },
+  watch: {
+    html: "src/**/*.{htm,html,php}",
+    js: "src/assets/js/**/*.js",
+    css: "src/assets/sass/**/*.scss",
+    images: "src/assets/i/**/*.{jpg,png,svg,gif,ico}",
+    fonts: "src/assets/fonts/**/*.{ttf,eot,woff,woff2}",
+    videos: "src/assets/videos/*"
+  },
+  clean: "./dist"
 };
-
 
 /* Tasks
 =========================*/
 
 function browserSync(done) {
-    browsersync.init({
-        server: {
-            baseDir: "./dist/"
-        },
-        port: 3000
-    });
-    done();
+  browsersync.init({
+    server: {
+      baseDir: "./dist/"
+    },
+    port: 3000
+  });
+  done();
 }
 
 function browserSyncReload(done) {
-    browsersync.reload();
-    done();
+  browsersync.reload();
+  done();
 }
 
 function html() {
-    return src(path.src.html, { base: 'src/' })
-        .pipe(plumber())
-        .pipe(dest(path.build.html))
-        .pipe(browsersync.stream());
+  return src(path.src.html, { base: "src/" })
+    .pipe(plumber())
+    .pipe(dest(path.build.html))
+    .pipe(browsersync.stream());
 }
 
 function css() {
-    return src(path.src.css, { base: './src/assets/sass/' })
-        .pipe(plumber())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer())
-        .pipe(cssbeautify())
-        .pipe(dest(path.build.css))
-        .pipe(cssnano({
-            zindex: false,
-            discardComments: {
-                removeAll: true
-            }
-        }))
-        .pipe(removeComments())
-        .pipe(rename({
-            suffix: ".min",
-            extname: ".css"
-        }))
-        .pipe(dest(path.build.css))
-        .pipe(browsersync.stream());
+  return src(path.src.css, { base: "./src/assets/sass/" })
+    .pipe(plumber())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autoprefixer())
+    .pipe(cssbeautify())
+    .pipe(dest(path.build.css))
+    .pipe(
+      cssnano({
+        zindex: false,
+        discardComments: {
+          removeAll: true
+        }
+      })
+    )
+    .pipe(removeComments())
+    .pipe(
+      rename({
+        suffix: ".min",
+        extname: ".css"
+      })
+    )
+    .pipe(dest(path.build.css))
+    .pipe(browsersync.stream());
 }
 
 function js() {
-    return src(path.src.js, { base: './src/assets/js/' })
-        .pipe(plumber())
-        .pipe(rigger())
-        .pipe(gulp.dest(path.build.js))
-        .pipe(uglify())
-        .pipe(rename({
-            suffix: ".min",
-            extname: ".js"
-        }))
-        .pipe(dest(path.build.js))
-        .pipe(browsersync.stream());
+  return src(path.src.js, { base: "./src/assets/js/" })
+    .pipe(plumber())
+    .pipe(rigger())
+    .pipe(
+      babel({
+        presets: ["@babel/env"]
+      })
+    )
+    .pipe(uglify())
+    .pipe(
+      rename({
+        suffix: ".min",
+        extname: ".js"
+      })
+    )
+    .pipe(dest(path.build.js))
+    .pipe(browsersync.stream());
 }
 
 function images() {
-    return src(path.src.images)
-        .pipe(dest(path.build.images));
+  return src(path.src.images).pipe(dest(path.build.images));
 }
 
 function fonts() {
-    return src(path.src.fonts)
-      .pipe(dest(path.build.fonts));
+  return src(path.src.fonts).pipe(dest(path.build.fonts));
 }
 
 function videos() {
-    return src(path.src.videos)
-      .pipe(dest(path.build.videos));
+  return src(path.src.videos).pipe(dest(path.build.videos));
 }
 
 function clean() {
-    return del(path.clean);
+  return del(path.clean);
 }
 
 function watchFiles() {
-    gulp.watch([path.watch.html], html);
-    gulp.watch([path.watch.css], css);
-    gulp.watch([path.watch.js], js);
-    gulp.watch([path.watch.images], images);
-    gulp.watch([path.watch.fonts], fonts);
-    gulp.watch([path.watch.videos], videos);
+  gulp.watch([path.watch.html], html);
+  gulp.watch([path.watch.css], css);
+  gulp.watch([path.watch.js], js);
+  gulp.watch([path.watch.images], images);
+  gulp.watch([path.watch.fonts], fonts);
+  gulp.watch([path.watch.videos], videos);
 }
 
-const build = gulp.series(clean, gulp.parallel(html, css, js, images, fonts, videos));
+const build = gulp.series(
+  clean,
+  gulp.parallel(html, css, js, images, fonts, videos)
+);
 const watch = gulp.parallel(build, watchFiles, browserSync);
-
 
 // export tasks
 exports.html = html;
